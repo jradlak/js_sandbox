d10bb152e216a257bce1e22ea20270adcbbaa9c4
Jakub Radlak <jradlak@gmail.com>
2015-04-19 11:51:56 +0200

    adding solution to http collect

---
+++ httpCollect2.js
@@ -0,0 +1,20 @@
+var bl = require('bl');
+var http = require('http');
+var args = process.argv;
+
+function collect(url, ready) {
+	http.get(url, function(res) {
+		res.setEncoding('utf8');
+		res.pipe(bl(function(err, data) {
+			console.log((data.toString()));
+			ready();
+		}));
+	});
+}
+
+collect(args[2], function ready () {
+	collect(args[3], function ready2 () {
+		collect(args[4], function ready3 () {
+		});	
+	});
+});
