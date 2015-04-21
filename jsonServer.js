802c0f586e7eaf748daba0bce30f6ab3647b5c2f
Jakub Radlak <jradlak@gmail.com>
2015-04-21 22:22:48 +0200

    working on solution to jsonServer

--- jsonServer.js
+++ jsonServer.js
@@ -7,17 +7,34 @@ var portNumber = process.argv[2];
 var server = http.createServer(function(req, res) {
 	if (req.method == 'GET') {
 		var urlParsed = url.parse(req.url, true);
-		
+
 		var pathName = urlParsed.pathname;
+		var query = urlParsed.query;
+		var qvalue = query.iso;
+
+		var result = undefined;
+
 		if (pathName === '/api/parsetime') {
-			var query = urlParsed.query;
-			console.log(query);
+			var timeStartIndex = qvalue.indexOf('T') + 1;
+			var timeEndIndex = qvalue.indexOf('.');
+			var time = qvalue.substring(timeStartIndex, timeEndIndex).split(':');
+
+			result = {
+				"hour" : time[0],
+				"minute" : time[1],
+				"second" : time[2]
+			}
 		}
-		/*
-		req.pipe(map(function (chunk) {
-			return chunk.toString().toUpperCase();
-		})).pipe(res);
-		*/
+
+		if (pathName === '/api/unixtime') {
+			var timeUnix = (new Date(qvalue)).getTime();
+			result = {
+				"unixtime" : timeUnix
+			}
+		}
+
+		res.writeHead(200, { 'Content-Type': 'application/json' })
+		res.end(JSON.stringify(result));
 	}
 });
 
