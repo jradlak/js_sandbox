0c5043088c91b9c918002eb9910c3c0812ff90b1
Jakub Radlak <jradlak@gmail.com>
2015-05-02 22:49:42 +0200

    solution to Duck Count

---
+++ functional/duckCount.js
@@ -0,0 +1,12 @@
+function duckCount() {
+  return (Array.prototype.slice.call(arguments, 0)).map(function(arg) {
+    return Object.prototype.hasOwnProperty.call(arg,'quack');
+  }).reduce(function(sum, isQuack) {
+    if (isQuack === true) sum++;
+    return sum;
+  }, 0);
+}
+
+// It works but better use filter than reduce!!!
+
+module.exports = duckCount
